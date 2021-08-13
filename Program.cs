6cc4ab8df0b686146d4df71493bfc8cb03531715
Andrey Morozov <andrey@morozov.lv>
2021-08-14 00:04:26 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
